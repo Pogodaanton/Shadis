6bc569ed6766ada2356e9dc7882b9307846406bc
Pogodaanton <8974778+Pogodaanton@users.noreply.github.com>
2020-01-19 15:35:30 +0100

    + auto-theming according to system defaults; documentation
    Apparently, my dolittle brain completely forgot about component lifecycles in React yesterday. At least it works now!

--- src/client/_DesignSystem/DesignSystem.tsx
+++ src/client/_DesignSystem/DesignSystem.tsx
@@ -6,18 +6,32 @@ import { ThemeName, Theme, props, state } from './DesignSystem.props';
 export const PogodaDesignToolkit = React.createContext({});
 
 const colorSchemes: { [key: string]: string } = {
-  'DARK': '(prefers-color-scheme: dark)',
-  'LIGHT': '(prefers-color-scheme: light)'
+  'dark': '(prefers-color-scheme: dark)',
+  'light': '(prefers-color-scheme: light)'
 }
 
 class PogodaDesignToolkitProvider extends Component<props, state> {
-  private handleUpdateTheme = (customThemeName?: ThemeName): void => {
-    const isLightMode: boolean = typeof customThemeName === "string" ? customThemeName === ThemeName.dark : this.state.contextData.theme === ThemeName.light;
+  // Dark / Light mode listeners
+  private activeMatches: Array<MediaQueryList> = [];
+
+  /**
+   * Toggles between dark / light mode.
+   * If a `ThemeName` is given, it will update to that specific one.
+   *
+   * @see https://git.io/JvkF1
+   * @memberof PogodaDesignToolkitProvider
+   */
+  handleUpdateTheme = (customThemeName?: ThemeName): void => {
+    // If ThemeName is given, we don't want to change theme to it's opposite.
+    // To avoid further complications in the code, we just pretend light mode were on if the opposite is the case 
+    const isLightMode: boolean = typeof customThemeName === "string"
+      ? customThemeName === ThemeName.dark
+      : this.state.contextData.theme === ThemeName.light;
+
     const updatedThemeColor: string = isLightMode ? Theme.dark : Theme.light;
     const updatedLuminance: number = isLightMode
         ? StandardLuminance.DarkMode
         : StandardLuminance.LightMode;
-    console.log(isLightMode)
 
     this.setState({
         contextData: {
@@ -32,42 +46,46 @@ class PogodaDesignToolkitProvider extends Component<props, state> {
     });
   };
 
-
-  constructor (props: props, state: state) {
-    super(props, state)
-    if (!window.matchMedia) {
+  /**
+   * Changes to dark / light mode according to system defaults
+   * @private
+   * @see https://git.io/JvkF6
+   * @event MediaQueryList
+   * @memberof PogodaDesignToolkitProvider
+   */
+  private matchMediaListener = (e: MediaQueryListEvent | MediaQueryList): any => {
+    if (!e || !e.matches) {
       return
     }
-
-    const listener = (e: MediaQueryListEvent | MediaQueryList): any => {
-      if (!e || !e.matches) {
-        return
-      }
-      const schemeNames = Object.keys(colorSchemes)
-      for (let i = 0; i < schemeNames.length; i++) {
-        const schemeName = schemeNames[i]
-        if (e.media === colorSchemes[schemeName]) {
-          this.handleUpdateTheme(schemeName === 'DARK' ? ThemeName.dark : ThemeName.light)
-          break
-        }
+    const schemeNames = Object.keys(colorSchemes)
+    for (let i = 0; i < schemeNames.length; i++) {
+      const schemeName = schemeNames[i]
+      if (e.media === colorSchemes[schemeName]) {
+        this.handleUpdateTheme(schemeName === 'dark' ? ThemeName.dark : ThemeName.light)
+        break
       }
     }
-
-    // Add listener for all themes
-    let activeMatches: Array<MediaQueryList> = []
-    Object.keys(colorSchemes).forEach(schemeName => {
-      const mq = window.matchMedia(colorSchemes[schemeName])
-      mq.addListener(listener)
-      activeMatches.push(mq)
-      listener(mq)
-    })
-
-    // Remove listeners, no memory leaks
-    activeMatches.forEach(mq => mq.removeListener(listener))
-    activeMatches = []
   }
 
-  public state = {
+  // Add OS theme listener
+  componentDidMount = () => {
+    if (!window.matchMedia) return
+    Object.keys(colorSchemes).forEach(schemeName => {
+      const mq = window.matchMedia(colorSchemes[schemeName])
+      mq.addListener(this.matchMediaListener)
+      this.activeMatches.push(mq)
+      this.matchMediaListener(mq)
+    })
+  }
+
+  // Remove theme listeners, no memory leaks
+  componentWillUnmount = () => {
+    if (!window.matchMedia) return
+    this.activeMatches.forEach(mq => mq.removeListener(this.matchMediaListener))
+    this.activeMatches = []
+  }
+
+  state = {
     contextData: {
       theme: ThemeName.light,
       toggleTheme: this.handleUpdateTheme
@@ -78,7 +96,7 @@ class PogodaDesignToolkitProvider extends Component<props, state> {
     }
   }
 
-  public render = (): ReactNode => (
+  render = (): ReactNode => (
     <PogodaDesignToolkit.Provider value={this.state.contextData}>
       <DesignSystemProvider designSystem={this.state.designSystem}>
         {this.props.children}

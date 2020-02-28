ebc604943e628565799169f81647405c6fdf0b51
Pogodaanton <8974778+Pogodaanton@users.noreply.github.com>
2020-02-28 18:33:21 +0100

    + Icon to Glyph conversion

--- src/client/_DesignSystem/index.ts
+++ src/client/_DesignSystem/index.ts
@@ -1,5 +1,6 @@
 import { Button, ButtonAppearance } from "./Button/Button";
 import Header from "./Header/Header";
 import Logo from "./Logo/Logo";
+import { iconToGlyph } from "./utilities";
 
-export { Button, Header, Logo, ButtonAppearance };
+export { Button, Header, Logo, ButtonAppearance, iconToGlyph };

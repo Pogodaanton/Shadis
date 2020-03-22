11d9d370d3176e46ebb615c551a390aa56a905e6
Pogodaanton <8974778+Pogodaanton@users.noreply.github.com>
2020-03-22 16:19:35 +0100

    ~ formatting, still

--- src/server/protected/config.sample.php
+++ src/server/protected/config.sample.php
@@ -3,36 +3,36 @@
 /**
  * MySQL hostname
  */
-define('DB_HOST', 'your_host_adress_here');
+define("DB_HOST", "your_host_adress_here");
 
 /**
  * MySQL database username
  */
-define('DB_USER', 'your_username_here');
+define("DB_USER", "your_username_here");
 
 /**
  * MySQL database password
  */
-define('DB_PASSWORD', 'your_password_here');
+define("DB_PASSWORD", "your_password_here");
 
 /**
  * The name of the database for Shadis
  * Accepts only numbers, letters, and underscores
  */
-define('DB_NAME', 'your_database_name_here');
+define("DB_NAME", "your_database_name_here");
 
 /**
  * Database Charset to use in creating database tables.
  */
-define('DB_CHARSET', 'utf8');
+define("DB_CHARSET", "utf8");
 
 /**
  * Token used for uploading if you are not logged in
  */
-define('UPLOAD_TOKEN', 'your_secret_upload_token_here');
+define("UPLOAD_TOKEN", "your_secret_upload_token_here");
 
 /**
  * Shadis Database Table prefix.
  * Accepts only numbers, letters, and underscores
  */
-$table_prefix = 'shadis_';
+$table_prefix = "shadis_";

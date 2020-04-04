ff6607b3cc4d38f2faacb29f0e4c2fc84408b2d1
Pogodaanton <8974778+Pogodaanton@users.noreply.github.com>
2020-04-04 18:05:54 +0200

    + lazy loading getAll

--- src/server/api/getAll.php
+++ src/server/api/getAll.php
@@ -3,12 +3,24 @@ require_once "../protected/db.inc.php";
 require_once "../protected/output.inc.php";
 session_start();
 
+// Request can only be GET
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+  error('Only GET request allowed!', 401);
+}
+
 // Login check
 if (!isset($_SESSION['u_id'])) {
   error('Unauthorized', 401);
 }
 
-$result = $db->request("SELECT id, thumb_height, timestamp, title FROM `" . $table_prefix . "files` ORDER BY timestamp");
+// Retrieve everything above this timestamp
+$since = $_GET["since"];
+
+if (isset($since)) {
+  $result = $db->request("SELECT id, thumb_height, timestamp, title FROM `" . $table_prefix . "files` WHERE timestamp > " . $since . " ORDER BY timestamp");
+} else {
+  $result = $db->request("SELECT id, thumb_height, timestamp, title FROM `" . $table_prefix . "files` ORDER BY timestamp");
+}
 
 header('Content-type: application/json');
 echo json_encode($result->fetch_all(MYSQLI_ASSOC));

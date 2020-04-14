9ba1fe1622269acafe72b2e8ceb35586c6bd237b
Pogodaanton <8974778+Pogodaanton@users.noreply.github.com>
2020-04-14 17:59:00 +0200

    ~ decoupled applyBackdropGradient

--- src/client/_DesignSystem/Header/Header.tsx
+++ src/client/_DesignSystem/Header/Header.tsx
@@ -4,43 +4,26 @@ import {
   DesignSystem,
   neutralLayerL3,
   neutralLayerL1,
-  acrylicNoise,
 } from "@microsoft/fast-components-styles-msft";
-import manageJss, { ComponentStyles, CSSRules } from "@microsoft/fast-jss-manager-react";
+import manageJss, { ComponentStyles } from "@microsoft/fast-jss-manager-react";
 import { parseColorHexRGBA } from "@microsoft/fast-colors";
-import { HeaderProps, HeaderClassNameContract, CustomApplyAcrylic } from "./Header.props";
+import { HeaderProps, HeaderClassNameContract } from "./Header.props";
 import Logo from "../Logo/Logo";
 import { Link } from "react-router-dom";
 import { classNames } from "@microsoft/fast-web-utilities";
-import { applyAcrylic } from "@microsoft/fast-jss-utilities";
+import { applyBackdropBackground } from "../utilities";
 
 /**
  * Generates an acrylic backdrop as well as a gradient background.
  */
-const applyBackdropBackground = (): CSSRules<DesignSystem> => {
-  /**
-   * Takes care of generating the gradient.
-   *
-   * @param opacityHex Opacity value of bottom color in Hexadecimal (2 chars)
-   */
-  const background = (opacityHex: string) => (designSystem: DesignSystem): string =>
+const applyBackdropGradient = applyBackdropBackground(
+  (opacityHex: string) => (designSystem: DesignSystem): string =>
     `linear-gradient(180deg, ${parseColorHexRGBA(
       neutralLayerL3(designSystem) + "ff"
     ).toStringWebRGBA()} 1%, ${parseColorHexRGBA(
       neutralLayerL1(designSystem) + opacityHex
-    ).toStringWebRGBA()} 98%)`;
-
-  /**
-   * We need to redeclare the type since it does not allow a function as backgroundColor
-   * even though that should work fine.
-   */
-  const customApplyAcrylic: CustomApplyAcrylic<DesignSystem> = applyAcrylic;
-  return customApplyAcrylic({
-    textureImage: acrylicNoise,
-    backgroundColor: background("b0"),
-    fallbackBackgroundColor: background("f7"),
-  });
-};
+    ).toStringWebRGBA()} 98%)`
+);
 
 const styles: ComponentStyles<HeaderClassNameContract, DesignSystem> = {
   header: {
@@ -50,7 +33,7 @@ const styles: ComponentStyles<HeaderClassNameContract, DesignSystem> = {
     width: "100%",
     padding: "0 12px",
     boxSizing: "border-box",
-    ...applyBackdropBackground(),
+    ...applyBackdropGradient,
   },
   header_fixed: {
     position: "fixed",
--- src/client/_DesignSystem/utilities.tsx
+++ src/client/_DesignSystem/utilities.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { IconType } from "react-icons/lib";
-import { CSSRules } from "@microsoft/fast-jss-manager-react";
-import { DesignSystem } from "@microsoft/fast-components-styles-msft";
+import {
+  CSSRules,
+  CSSRuleResolver,
+  CSSStaticRule,
+} from "@microsoft/fast-jss-manager-react";
+import { DesignSystem, acrylicNoise } from "@microsoft/fast-components-styles-msft";
+import { applyAcrylic } from "@microsoft/fast-jss-utilities";
+import { CustomApplyAcrylic } from "./Header/Header.props";
 
 /**
  * Converts an icon from "react-icons" into a glyph that can accept JSS-managed-classes
@@ -14,6 +20,9 @@ export const iconToGlyph = (Icon: IconType) => (className: string): React.ReactN
   <Icon className={className} />
 );
 
+/**
+ * Appends CSS parameters that create a centered flexbox
+ */
 export const applyCenteredFlexbox = (): CSSRules<DesignSystem> => {
   return {
     display: "flex",
@@ -21,4 +30,29 @@ export const applyCenteredFlexbox = (): CSSRules<DesignSystem> => {
     alignItems: "center",
     justifyContent: "center",
   };
+};
+
+/**
+ * Generates an acrylic backdrop with the given background functon.
+ *
+ * Since not every browser supports acrylic materials, the background
+ * function needs to accept different transparency values.
+ *
+ * @param background Function that accpets an opacity value in Hexadecimal (2 chars)
+ */
+export const applyBackdropBackground = (
+  background: (opacityHex: string) => CSSRuleResolver<DesignSystem> | CSSStaticRule,
+  opacityHex: string = "b0",
+  fallbackOpacityHex: string = "f7"
+): CSSRules<DesignSystem> => {
+  /**
+   * We need to redeclare the type since it does not allow a function as backgroundColor
+   * even though that should work fine.
+   */
+  const customApplyAcrylic: CustomApplyAcrylic<DesignSystem> = applyAcrylic;
+  return customApplyAcrylic({
+    textureImage: acrylicNoise,
+    backgroundColor: background(opacityHex),
+    fallbackBackgroundColor: background(fallbackOpacityHex),
+  });
 };

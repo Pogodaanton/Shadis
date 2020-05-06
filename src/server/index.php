3ed7721d7aa15ae35df35fcc0f109f1deefb176a
Pogodaanton <8974778+Pogodaanton@users.noreply.github.com>
2020-05-06 17:37:57 +0200

    + Metatags

--- src/server/index.php
+++ src/server/index.php
@@ -4,11 +4,14 @@ session_start();
 $uri_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $segments = explode('/', trim($uri_path, '/'));
 $file_data = null;
+$title = "Shadis";
 
 if (!empty($segments[0])) {
   if (strlen($segments[0]) === 8) {
     require_once "./protected/db.inc.php";
+    require_once "./protected/output.inc.php";
     $file_data = $db->request_file($segments[0]);
+    $title = ($file_data["title"] !== "" ? ($file_data["title"] . " - ") : "") . $file_data["id"] . " - Shadis";
   }
 }
 ?>
@@ -20,7 +23,7 @@ if (!empty($segments[0])) {
   <link rel="icon" href="%PUBLIC_URL%/static/media/favicon.ico" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta name="theme-color" content="#000000" />
-  <meta name="description" content="Web site created using create-react-app" />
+  <meta content="noindex" name="robots">
   <link rel="apple-touch-icon" href="%PUBLIC_URL%/static/media/logo192.png" />
   <!--
     manifest.json provides metadata used when your web app is installed on a
@@ -37,6 +40,8 @@ if (!empty($segments[0])) {
     Learn how to configure a non-root public URL by running `npm run build`.
   -->
   <title>Shadis</title>
+  <meta name="title" content="<?php echo $title; ?>">
+  <meta name="description" content="Share and host your favourite screenshots and screencaptures on your own server!">
   <?php
   if (isset($_SESSION["u_id"])) {
     $user_data = array("username" => $_SESSION["u_name"]);
@@ -46,6 +51,9 @@ if (!empty($segments[0])) {
   }
   if (!is_null($file_data)) :
     $file_data["fromServer"] = true;
+    $origin_url = url_origin($_SERVER);
+    $file_url = $origin_url . "/" . $file_data["id"] . "." . $file_data["extension"];
+
     echo '<script>var fileData = ';
     echo json_encode($file_data);
     echo '</script>';
@@ -68,6 +76,22 @@ if (!empty($segments[0])) {
         object-fit: scale-down;
       }
     </style>
+    <link rel="image_src" href="<?php echo $file_url; ?>">
+
+    <!-- Facebook OpenGraph Metadata -->
+    <meta property="og:title" content="Shadis">
+    <meta property="og:site_name" content="Shadis">
+    <meta property="og:description" content="<?php echo $file_data["title"]; ?>">
+    <meta property="og:image" content="<?php echo $file_url; ?>">
+    <meta property="og:image:width" content="<?php echo $file_data["width"]; ?>">
+    <meta property="og:image:height" content="<?php echo $file_data["height"]; ?>">
+    <meta property="og:url" content="<?php echo $origin_url . "/" . $file_data["id"] . "/"; ?>">
+
+    <!-- Twitter Metadata -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Image on Shadis">
+    <meta name="twitter:description" content="<?php echo $file_data["title"]; ?>">
+    <meta name="twitter:image" content="<?php echo $file_url; ?>">
   <?php endif; ?>
 </head>
 
@@ -76,7 +100,7 @@ if (!empty($segments[0])) {
   <div id="root"></div>
   <?php if (!is_null($file_data)) : ?>
     <div id="preContainer">
-      <img src="%PUBLIC_URL%/uploads/<?php echo $file_data["id"] . "." . $file_data["extension"] ?>" />
+      <img src="<?php echo $file_url; ?>" />
     </div>
   <?php endif; ?>
 </body>

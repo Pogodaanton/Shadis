7763e7a677d5629e7e95e81cbccbfc01420fe3e0
Pogodaanton <8974778+Pogodaanton@users.noreply.github.com>
2020-06-20 12:58:24 +0200

    play indicator now supports dark-mode

--- src/DashboardList/views/DashboardListCell.tsx
+++ src/DashboardList/views/DashboardListCell.tsx
@@ -14,6 +14,7 @@ import {
   ElevationMultiplier,
   applyElevatedCornerRadius,
   applyPillCornerRadius,
+  neutralFillInputRest,
 } from "@microsoft/fast-components-styles-msft";
 import { useTranslation } from "react-i18next";
 import {
@@ -129,10 +130,10 @@ const styles: ComponentStyles<DashboardListCellClassNameContract, DesignSystem> 
     top: "10px",
     left: "10px",
     margin: "auto",
-    color: "#fff",
+    color: neutralFillInputRest,
     background: neutralForegroundRest,
     opacity: "0.7",
-    filter: "drop-shadow(0px 0px 1px black)",
+    filter: des => `drop-shadow(0px 0px 1px ${neutralFillInputRest(des)})`,
     ...applyPillCornerRadius(),
   },
 };
